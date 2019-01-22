bd0dbde9a41fbb586e24568ccc8de83decda3eba
Mr.Maple <daijiale@users.noreply.github.com>
2019-01-22 17:24:56 +0800

    update

--- src/Home/Page3.jsx
+++ src/Home/Page3.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-console */
 import 'react-html5video/dist/styles.css';
-import { Modal } from 'antd-mobile';
+import { Modal, Button } from 'antd-mobile';
 import React from 'react';
 import TweenOne from 'rc-tween-one';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
@@ -34,10 +34,12 @@ export default class Page3 extends React.PureComponent {
         uri: '',
       },
     choiceMethod: '',
+    lastURI: '',
   };
 
   async componentDidMount() {
     // 加载首页数据
+
     await this.initVideoInfoFromServer();
     // 实例化hls，支持m3u8播放
     this.addHlsToPlayer();
@@ -49,6 +51,8 @@ export default class Page3 extends React.PureComponent {
     if (!baseInfoRes.data.ErrMsg === '') {
       return;
     }
+    // 记录用户上次所看片集位置，以便进行续播
+    this.setState({ lastURI: baseInfoRes.data.Result.LastURI });
     await this.resolveVideoInfo(baseInfoRes.data.Result.FirstURI);
   }
 
@@ -58,13 +62,12 @@ export default class Page3 extends React.PureComponent {
     if (!videoRes.data.ErrMsg === '') {
       return;
     }
-    console.log('服务端video init数据');
     const videoInfoRes = videoRes.data.Result;
-    if (!videoInfoRes.NoNeedPaid) {
-      alertM('未购买电影，请先支付');
-      // TODO:弹出支付页选项
-    }
-    const videoInfoCache = '';
+    // if (!videoInfoRes.NoNeedPaid) {
+    //   alertM('未购买电影，请先支付');
+    //   // TODO:弹出支付页选项
+    // }
+    const videoInfoCache = {};
     videoInfoCache.VideoPlayURI = videoInfoRes.VideoPlayURI;
     videoInfoCache.choiceMethod = videoInfoRes.ChoiceMethod;
     if (videoInfoRes.ChoiceMethod === 'Single') {
@@ -80,6 +83,7 @@ export default class Page3 extends React.PureComponent {
       choiceMethod: videoInfoCache.choiceMethod,
     });
     console.log(this.state);
+    this.addHlsToPlayer();
   }
 
   async handleFirPress() {
@@ -110,7 +114,7 @@ export default class Page3 extends React.PureComponent {
     }
   }
 
-  async handleVideoEnded() {
+  handleVideoEnded() {
     if (
       document.body.scrollHeight === window.screen.height
       && document.body.scrollWidth === window.screen.width
@@ -120,24 +124,30 @@ export default class Page3 extends React.PureComponent {
     if (this.state.choiceMethod === 'Single') {
       // 单个文字选项
       alertM('选择', '选择题介绍', [
-        { text: '取消', onPress: await this.handleFirPress() },
-        { text: '确定', onPress: await this.handleSecPress() },
+        { text: '取消', onPress: () => { this.handleFirPress(); } },
       ]);
-    } else if (this.state.choiceMethod === 'Select') {
-      // 文字双选
-      alertM('选择', '选择题介绍', [
-        { text: '取消', onPress: await this.handleFirPress() },
-      ]);
-    } else if (this.state.choiceMethod === 'Picture select') {
+    } else if (this.state.choiceMethod === 'PictureSelect') {
       alertM('图片双选');
     } else if (this.state.choiceMethod === 'Inbox') {
       alertM('输入框双选');
+    } else {
+      console.log('文字双选');
+      // 文字双选
+      alertM('选择', '选择题介绍', [
+        { text: '取消', onPress: () => { this.handleFirPress(); } },
+        { text: '确定', onPress: () => { this.handleSecPress(); } },
+      ]);
     }
   }
 
+  async getLastURIVideo(lastURI) {
+    await this.resolveVideoInfo(lastURI);
+  }
 
   render() {
     const { isMobile } = this.props;
+    // const lastURI = this.state.lastURI;
+    const lastURI = 'http://192.168.5.141:8080/v1/video_info/2';
     const children = this.state.videoInfo.map((item, i) => (
       <Element key={i.toString()}>
         <BgElement className="banner" key="bg">
@@ -155,6 +165,7 @@ export default class Page3 extends React.PureComponent {
         </BgElement>
       </Element>
     ));
+
     const childrenToRender = (
       <TweenOne
         key="banner-wrapper"
@@ -170,6 +181,7 @@ export default class Page3 extends React.PureComponent {
         </BannerAnim>
       </TweenOne>
     );
+
     return (
       <div className="page-wrapper page3">
         <div className="page">
@@ -181,6 +193,7 @@ export default class Page3 extends React.PureComponent {
             childrenToRender
           )}
         </div>
+        <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />} onClick={() => { this.getLastURIVideo(lastURI); }}>继续上次观看</Button>
       </div>
     );
   }
